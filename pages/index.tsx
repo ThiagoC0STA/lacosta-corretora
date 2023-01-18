eda335285d88fbc25412bdc8c4586d9b15d7d2e4
Thiago Rodrigues da Costa <82855801+ThiagoC0STA@users.noreply.github.com>
2023-01-18 14:05:36 -0300

    first commit

---
+++ pages/index.tsx
@@ -0,0 +1,21 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+
+const Home: NextPage = () => {
+  return (
+    <>
+      <Head>
+        <title>Lacosta Corretora</title>
+        <meta
+          name="description"
+          content="A Lacosta Corretora oferece soluções personalizadas de seguros para garantir a tranquilidade de você e sua família. Entre em contato conosco e solicite agora uma cotação "
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <h1>Lacosta Corretora</h1>
+    </>
+  );
+};
+
+export default Home;
