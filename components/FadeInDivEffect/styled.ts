2b686133f42fa33e8dac1cdb3026b0bb31370a0e
Thiago Rodrigues da Costa <82855801+ThiagoC0STA@users.noreply.github.com>
2023-02-06 18:07:09 -0300

    fade-in changes

--- components/FadeInDivEffect/styled.ts
+++ components/FadeInDivEffect/styled.ts
@@ -3,7 +3,7 @@ import styled from "styled-components";
 export const FadeInDiv = styled.div<{ show: boolean }>`
   opacity: 0;
   transform: translateY(5%);
-  transition: all 0.5s ease-in 0.7s;
+  transition: all 0.5s ease-in 0.4s;
 
   ${({ show }) =>
     show &&
